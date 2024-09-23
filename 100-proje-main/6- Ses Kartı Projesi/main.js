8dbdfa97e3b2badd39e7f3e14efac59759a95f18
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-09-23 12:06:48 +0300

    ..

--- 100-proje-main/6- Ses Kartı Projesi/main.js
+++ 100-proje-main/6- Ses Kartı Projesi/main.js
@@ -5,20 +5,18 @@ const buttons = document.getElementById("buttons");
 const stopSound = () => {
   sounds.forEach((sound) => {
     const currentSound = document.getElementById(sound);
-    currentSound.pause();
+    currentSound.pause()
     currentSound.currentTime = 0;
-  });
-};
+  })
+}
 /* button oluşturma bölümü*/
 sounds.forEach((sound) => {
   const btn = document.createElement("button");
   btn.classList.add("btn");
   btn.innerHTML = sound;
   btn.addEventListener("click", () => {
-    stopSound();
-    document.getElementById(sound).play();
-  });
-  buttons.appendChild(btn);
-});
-
-
+    stopSound()
+    document.getElementById(sound).play()
+  })
+  buttons.appendChild(btn)
+})
---
+++ 100-proje-main/README.md
@@ -0,0 +1,20 @@
+# 100 proje
+ 100 proje yaparak  pratik ve gelişme noktasında yola devam 
+
+ 1- Slider_Panel
+ https://www.youtube.com/watch?v=vRotFcYUFq0
+
+ 2- Search 
+ https://www.youtube.com/watch?v=P5KGL8scGBA
+
+ 3- Fixed Navbar
+ https://youtu.be/ubt2qizNNwc
+
+
+4- Login Animasyonlu
+https://youtu.be/wPoKX53iRzg
+
+.......
+
+projelerin tamamı
+https://www.youtube.com/@alyaduanaga9242
