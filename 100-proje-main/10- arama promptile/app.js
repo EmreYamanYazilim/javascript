f599a4a72dfb95bad3f544b8743bcaa7d36ef80f
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-10-26 23:15:14 +0300

    .

--- 100-proje-main/10- arama promptile/app.js
+++ 100-proje-main/10- arama promptile/app.js
@@ -1,29 +1,29 @@
 let urun1 = {
     isim: "ACER Swift",
     kategori: "Teknoloji",
-    fiyat: 6.219
+    fiyat: 26.219
 }
 
 let urun2 = {
     isim: "ACER Nitro 5",
     kategori: "Teknoloji",
-    fiyat: 15.475
+    fiyat: 35.475
 }
 
 let urun3 = {
     isim: "LENOVO V15",
     kategori: "Teknoloji",
-    fiyat: 10.999
+    fiyat: 30.999
 }
 let urun4 = {
     isim: "LENOVO V14",
     kategori: "Teknoloji",
-    fiyat: 4.399
+    fiyat: 24.399
 }
 let urun5 = {
     isim: "LENOVO Ideapad",
     kategori: "Teknoloji",
-    fiyat: 4.510
+    fiyat: 24.510
 }
 let urunler = [urun1, urun2, urun3, urun4, urun5];
 let filtreliUrunler = [];
